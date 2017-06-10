e9c398510a5557990a9c79e36fbdaddecae93561
roopkumard19 <roopkumard19@github.com>
2017-06-10 14:07:08 -0700

    made it simple

--- access_point.sh
+++ access_point.sh
@@ -1,61 +1,26 @@
 #!/bin/bash
-
-# WLAN parameters
-SSID="Docker"
-HW_MODE="g"
-CHANNEL="6"
-PASSPHRASE="docker123"
-WPA_MODE="WPA-PSK"
-
-# Other parameters
-SUBNET="192.168.0"
-IP_AP="192.168.0.1"
-NETMASK="/24"
-DNS_SERVER="8.8.8.8"
-DOCKER_NAME="ap-container"
-CONF_FILE="hotspot-config.txt"
-PATHSCRIPT=`pwd`
-DOCKER_IMAGE=""
-ROOT_UID="0"
-
-if [ "$UID" -ne "$ROOT_UID" ] ; then
-    echo "You must be root to run this script!"
-    exit 1
-fi
-
-init () {
-
-}
-
-start () {
-
-}
-
-stop () {
-
-}
-
-
-if [ "$1" == "start" ]
+#Initial wifi interface configuration
+ifconfig $1 up 10.0.0.1 netmask 255.255.255.0
+sleep 2
+ 
+###########Start dnsmasq, modify if required##########
+if [ -z "$(ps -e | grep dnsmasq)" ]
 then
-    if [[ -z "$2" ]]
-    then
-        echo -e "[ERROR] No interface provided. Exiting..."
-        exit 1
-    fi
-    IFACE=${2}
-    stop "$IFACE"
-    clear    
-    init "$IFACE"
-    start "$IFACE"
-elif [ "$1" == "stop" ]
-then
-    if [[ -z "$2" ]]
-    then
-        echo -e "[ERROR] No interface provided. Exiting..."
-        exit 1
-    fi
-    IFACE=${2}
-    stop "$IFACE"
+ dnsmasq
 fi
+###########
+ 
+#Enable NAT
+iptables --flush
+iptables --table nat --flush
+iptables --delete-chain
+iptables --table nat --delete-chain
+iptables --table nat --append POSTROUTING --out-interface $2 -j MASQUERADE
+iptables --append FORWARD --in-interface $1 -j ACCEPT
+ 
+sysctl -w net.ipv4.ip_forward=1
+ 
+#start hostapd
+hostapd /etc/hostapd/hostapd.conf 1> /dev/null
+killall dnsmasq
 
